09748f8585c4aa3cc125fa5384316fb11b6d16e3
Rafael Lima <rafaelima1250@gmail.com>
2026-08-26 14:51:36 -0300

    Refactor line_functions.py for clarity and functionality

--- line_codes/line_functions.py
+++ line_codes/line_functions.py
@@ -5,6 +5,7 @@ driver_r = None
 driver_l = None
 motors = None
 odometry = None
+last_line_time = time.monotonic()
 
 
 def set_robot_context(right_driver, left_driver, robot_motors, robot_odometry):
@@ -26,12 +27,14 @@ def both_drivers_stop():
     driver_l.stop()
 
 
-def is_clear_intersection(digital):
-    return all(digital)
-
-
-def is_left_90_candidate(digital):
-    return digital[0] and digital[1] and not digital[3] and not digital[4]
+def is_curve_90_candidate(digital):
+    return (
+    (digital[0] and digital[1] and  digital[2] and not digital[3] and not digital[4]) #Left
+    or 
+    (digital[3] and digital[4] and digital[2] and not digital[0] and not digital[1]) #Right
+    or
+    (all(digital))
+    )
 
 
 def is_green(color):
@@ -50,12 +53,12 @@ def detect_color_marking(color_sensor_r, color_sensor_l):
 
     both_drivers_set_speed(10, 10)
 
-    right_time = 0.2
+    right_time = 0.45
     left_time = right_time * 2
 
     start_time = time.monotonic()
 
-    while time.monotonic() - start_time < 0.2:
+    while time.monotonic() - start_time < 0.3:
         color_r = color_sensor_r.get_color()
         color_l = color_sensor_l.get_color()
 
@@ -128,12 +131,15 @@ def detect_color_marking(color_sensor_r, color_sensor_l):
     return None
 
 
-def is_right_90_candidate(digital):
-    return digital[3] and digital[4] and not digital[0] and not digital[1]
 
+def is_gap(digital):
+    global last_line_time
 
-def is_gap(reading):
-    return not reading["line_detected"]
+    if any(digital):
+        last_line_time = time.monotonic()
+        return False
+
+    return time.monotonic() - last_line_time >= 0.2
 
 
 def update_odometry_motors():
@@ -181,95 +187,87 @@ def turn_right(target_angle_rad):
 
 
 def move_straight_for(duration, speed):
-    start_time = time.monotonic()
-
-    while time.monotonic() - start_time < duration:
-        both_drivers_set_speed(speed, speed)
-
+    both_drivers_set_speed(speed, speed)
+    time.sleep(duration)
 
     both_drivers_stop()
-
-
-def center_stays_on_line_during_short_forward(line_sensor):
-    both_drivers_set_speed(30, 30)
-
-    start_time = time.monotonic()
-
-    while time.monotonic() - start_time < 0.25:
-        reading = line_sensor.get_data()
-        digital = reading["digital"]
-
-        if digital[2]:
-            both_drivers_stop()
-            return True
-
-    right_turn_time = 1.2
-
-    both_drivers_set_speed(-30, 30)
-
-    start_time = time.monotonic()
-
-    while time.monotonic() - start_time < right_turn_time:
-        reading = line_sensor.get_data()
-        digital = reading["digital"]
-
-        if digital[2]:
-            both_drivers_stop()
-            return True
-
-    left_turn_time = right_turn_time * 2
-
-    both_drivers_set_speed(30, -30)
-
-    start_time = time.monotonic()
-
-    while time.monotonic() - start_time < left_turn_time:
-        reading = line_sensor.get_data()
-        digital = reading["digital"]
-
-        if digital[2]:
-            both_drivers_stop()
-            return True
-
-    both_drivers_set_speed(-30, 30)
-
-    time.sleep(right_turn_time)
-
-    both_drivers_stop()
-
-    return False
 
 
 def handle_intersection():
     move_straight_for(0.3, 20)
 
+def ways_check(line_sensor):
+    right = False
+    left = False
+    front = False
+    ways = 0
+    right_curve = 1.55
+    left_curve = right_curve * 2
 
-def handle_left_candidate(line_sensor):
-    print("Handling left candidate", flush=True)
-    center_stayed = center_stays_on_line_during_short_forward(line_sensor)
+    centralize_on_line(line_sensor)
 
-    if center_stayed:
-        print("Center stayed on line during short forward", flush=True)
-        both_drivers_set_speed(30, 30)
-        time.sleep(0.35)
-    else:
-        print("Center did not stay on line, turning left", flush=True)
+    move_straight_for(0.85, 20)
+
+    reading = line_sensor.get_data()
+    digital = reading["digital"]
+
+    if any(digital):
+        front = True
+        ways += 1
+
+    print(front, flush=True)
+
+    both_drivers_set_speed(30, -30)
+    time.sleep(right_curve)
+
+    reading = line_sensor.get_data()
+    digital = reading["digital"]
+
+    if any(digital):
+        right = True
+        ways += 1
+
+    print(right, flush=True)
+
+    both_drivers_set_speed(-30, 30)
+    time.sleep(left_curve)
+
+    reading = line_sensor.get_data()
+    digital = reading["digital"]
+
+    if any(digital):
+        left = True
+        ways += 1
+
+    print(left, flush=True)
+
+    if ways != 1:
         both_drivers_set_speed(30, -30)
-        time.sleep(1.4)
+        time.sleep(right_curve)
+        centralize_on_line(line_sensor)
+        move_straight_for(0.25, 30)
+        print("Green check is necessary", flush=True)
+        return True
+    elif front:
+        both_drivers_set_speed(30, -30)
+        time.sleep(right_curve)
+        centralize_on_line(line_sensor)
+    elif left:
+        centralize_on_line(line_sensor)
+    elif right:
+        both_drivers_set_speed(30, -30)
+        time.sleep(right_curve*2)
+        centralize_on_line(line_sensor)
 
 
-def handle_right_candidate(line_sensor):
-    print("Handling right candidate", flush=True)
-    center_stayed = center_stays_on_line_during_short_forward(line_sensor)
+def handle_curve_candidate(line_sensor, color_sensor_r, color_sensor_l):
+    print("Handling curve candidate", flush=True)
+    needs_green_check = ways_check(line_sensor)
 
-    if center_stayed:
-        print("Center stayed on line during short forward", flush=True)
-        both_drivers_set_speed(30, 30)
-        time.sleep(0.35)
-    else:
-        print("Center did not stay on line, turning right", flush=True)
-        both_drivers_set_speed(-30, 30)
-        time.sleep(1.4)
+    if needs_green_check:
+        color_marking = detect_color_marking(color_sensor_r, color_sensor_l)
+
+        handle_color_marking(color_marking)
 
 
 def handle_color_marking(color_marking):
@@ -304,6 +302,7 @@ def follow_line(reading, pid, base_speed):
 
     correction = pid.calculate(position)
 
+    print(pid.get_pid())
     left_speed = base_speed + correction
     right_speed = base_speed - correction
 
@@ -314,14 +313,15 @@ def follow_line(reading, pid, base_speed):
 
 
 def try_cross_gap(line_sensor):
+    print("Trying to cross gap", flush=True)
     start_time = time.monotonic()
 
-    while time.monotonic() - start_time < 3.0:
-        both_drivers_set_speed(30, 30)
-
+    both_drivers_set_speed(30, 30)
+    while time.monotonic() - start_time < 2.0:
         reading = line_sensor.get_data()
+        digital = reading["digital"]
 
-        if reading["line_detected"]:
+        if any(digital):
             both_drivers_stop()
             return True
 
@@ -342,16 +342,16 @@ def is_obstacle(distance_sensor):
 
 def handle_lost_line(line_sensor, last_position):
 
-    if last_position < 0:
-        left_speed = 15
+    if last_position > 0:
+        left_speed = -30
         right_speed = 30
     else:
         left_speed = 30
-        right_speed = 15
+        right_speed = -30
+
+    both_drivers_set_speed(left_speed, right_speed)
 
     while True:
-        both_drivers_set_speed(left_speed, right_speed)
-
         reading = line_sensor.get_data()
         digital = reading["digital"]
 
@@ -431,6 +431,50 @@ def handle_obstacle(line_sensor):
     both_drivers_stop()
 
     return False
+
+def centralize_on_line(line_sensor):
+    move_straight_for(0.6, -20)
+    time.sleep(0.1)
+
+    both_drivers_set_speed(-30, 30)
+
+    start_time = time.monotonic()
+    right_turn_time = 0.4
+
+    while time.monotonic() - start_time < right_turn_time:
+        reading = line_sensor.get_data()
+        digital = reading["digital"]
+
+        if digital[2]:
+            both_drivers_stop()
+            time.sleep(0.1)
+            move_straight_for(0.3, 20)
+            return True
+
+    left_turn_time = right_turn_time * 2
+
+    both_drivers_set_speed(30, -30)
+
+    start_time = time.monotonic()
+
+    while time.monotonic() - start_time < left_turn_time:
+        reading = line_sensor.get_data()
+        digital = reading["digital"]
+
+        if digital[2]:
+            both_drivers_stop()
+            time.sleep(0.1)
+            move_straight_for(0.3, 20)
+            return True
+
+    both_drivers_set_speed(-30, 30)
+
+    time.sleep(right_turn_time)
+
+    both_drivers_stop()
+
+    return False
+
 
 
 def handle_180():
